1a07a17227847bcdb1fca08693aa1c85a8c9d6ca
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2024-09-01 14:42:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (16.74%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,20 +1,44 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-         Stack<Character> stack = new Stack<>();
+        //  Stack<Character> stack = new Stack<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     if (ch == '(') {
+        //         stack.push(ch);
+        //     } else {
+        //         //insert incase  of invalid pair }} or close comes before open
+        //         if (stack.isEmpty() || stack.peek() == ')') {
+        //             stack.push(ch);
+        //         } else {
+        //             //pop in case of valid parenthesis
+        //             stack.pop();
+        //         }
+        //     }
+        // }
+        // return stack.size();
+
+
+
+         int open =0, close=0;
+        // Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch == '(') {
-                stack.push(ch);
+               // stack.push(ch);
+                open++;
             } else {
                 //insert incase  of invalid pair }} or close comes before open
-                if (stack.isEmpty() || stack.peek() == ')') {
-                    stack.push(ch);
+              //  if (stack.isEmpty() || stack.peek() == '}') {
+                if(open<=0 ){
+                   // stack.push(ch);
+                    close++;
                 } else {
                     //pop in case of valid parenthesis
-                    stack.pop();
+                   // stack.pop();
+                    open--;
                 }
             }
         }
-        return stack.size();
+        return (open+close); //stack.size();
     }
-}
+    }
