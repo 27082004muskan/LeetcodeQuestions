79ad98b717b224d3254773e8aaa71322e6b13a23
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-02-26 14:07:02 +0530

    Time: 2 ms (98.86%) | Memory: 45.2 MB (29.30%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,49 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] temp =new int[2];
-        for(int i =0;i<nums.length-1;i++){
-            for(int j = i+1;j<nums.length;j++){
-              if(nums[i]+nums[j]==target){
-             temp[0] = i;
-                    temp[1] = j;
-                    return temp;
-              }
+           int[] temp =new int[2];
+        // for(int i =0;i<nums.length-1;i++){
+        //     for(int j = i+1;j<nums.length;j++){
+        //       if(nums[i]+nums[j]==target){
+        //      temp[0] = i;
+        //             temp[1] = j;
+        //             return temp;
+        //       }
+        //     }
+        // }
+        // return temp;
+      
+
+      // METHOD-2 
+    // int n = nums.length;
+    // int i = 0, j = n - 1;  // Two pointers
+
+    // while (i < j) {
+    //     int sum = nums[i] + nums[j];
+
+    //     if (sum == target) {
+    //         temp[0] = i;
+    //         temp[1] = j;
+    //         return temp; 
+    //     } else if (sum < target) {
+    //         i++;  // Move left pointer forward to increase sum
+    //     } else {
+    //         j--;  // Move right pointer backward to decrease sum
+    //     }
+    // }
+    // return new int[] {};  // Return empty array if no solution found
+
+Map<Integer , Integer> map =new HashMap<>();
+for(int i =0;i<nums.length;i++){
+ int complement = target-nums[i];
+
+
+  if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
-        }
-        return temp;
+     map.put(nums[i], i);
+
+    }
+    return new int[]{-1, -1};
     }
 }
+
