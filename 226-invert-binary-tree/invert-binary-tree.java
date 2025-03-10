1dc0fe4476f4fa51c73c9ebb9230215cb685fd62
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-03-10 23:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (8.39%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -1,25 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//        if(root ==null) return null ;
+ 
+//     final Queue<TreeNode> queue = new LinkedList<>();
+//     queue.add(root);
+
+//     while(!queue.isEmpty()){
+//         final TreeNode temp = queue.poll() ;
+
+//         // swap nodes 
+//         final TreeNode temp =TreeNode.left;
+//          TreeNode.left = TreeNode.right;
+//         TreeNode.right = temp;
+
+//         if(TreeNode.left!=null)  queue.add(TreeNode.left);
+//            if(TreeNode.right!=null)  queue.add(TreeNode.right);
+
+//     }
+//     return root ; 
+//     }
+// }
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-       if(root ==null) return null ;
-       TreeNode left=invertTree(root.left);
-        TreeNode right=invertTree(root.right);
-        root.left=right;
-        root.right=left;
-           return root ;
+        if(root==null || (root.left==null && root.right==null)){
+            return root;
+        }
+        TreeNode temp =root.left;
+        root.left=root.right;
+        root.right=temp;
+
+        root.left=invertTree(root.left);
+        root.right=invertTree(root.right);
+        return root ;
     }
 }
