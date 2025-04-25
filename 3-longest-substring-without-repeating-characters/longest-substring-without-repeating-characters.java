766351cb987542773f223e3f2c0b8923e3a4a950
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-04-25 14:52:32 +0530

    Time: 6 ms (70.36%) | Memory: 44.3 MB (82.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,45 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//          int i =0 , j =0;
+//          int maxLen= Integer.MIN_VALUE;
+//          HashSet<Character> set = new HashSet<>();
+//          int l =0;
+//          while(j<n){
+//            char ch =  s.charAt(j);
+//              if(set.contains(ch)){
+//                 while(i<j && set.contains(ch)){
+//                     set.remove(s.charAt(i));
+//                     i++;
+//                 }
+              
+//              }
+//                set.add(ch);
+//                 maxLen=Math.max(maxLen , j-i+1);
+//                 j++;
+//          }
+//          return maxLen==Integer.MIN_VALUE ? 0 : maxLen;
+//     }
+// }
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+
         int n = s.length();
          int i =0 , j =0;
          int maxLen= Integer.MIN_VALUE;
-         HashSet<Character> set = new HashSet<>();
+         HashMap<Character , Integer> map = new HashMap<>();
          int l =0;
+
          while(j<n){
-           char ch =  s.charAt(j);
-             if(set.contains(ch)){
-                while(i<j && set.contains(ch)){
-                    set.remove(s.charAt(i));
-                    i++;
-                }
+            char ch = s.charAt(j);
+            if(map.containsKey(ch) && map.get(ch)>=i){
+              i=map.get(ch)+1; }
               
-             }
-               set.add(ch);
-                maxLen=Math.max(maxLen , j-i+1);
-                j++;
+              map.put(ch , j);
+            
+            maxLen=Math.max(maxLen , j-i+1);
+            j++;
          }
          return maxLen==Integer.MIN_VALUE ? 0 : maxLen;
     }
