0023cfdfbb704a57320ddc50f961104ac01f4c7d
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-05-14 17:17:33 +0530

    Time: 5 ms (84.75%) | Memory: 44.6 MB (86.18%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,22 +1,42 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        int[] mpp = new int[10];
-        for (int d : digits) mpp[d]++;
-        List<Integer> res = new ArrayList<>();
-        for (int i = 1; i <= 9; i++) {
-            if (mpp[i] == 0) continue;
-            mpp[i]--;
-            for (int j = 0; j <= 9; j++) {
-                if (mpp[j] == 0) continue;
-                mpp[j]--;
-                for (int k = 0; k <= 8; k += 2) {
-                    if (mpp[k] == 0) continue;
-                    res.add(i*100 + j*10 + k);
-                }
-                mpp[j]++;
-            }
-            mpp[i]++;
+        List<Integer> result = new ArrayList<>();
+        int[] freq = new int[10];
+
+        for (int digit : digits) {
+            freq[digit]++;
         }
-        return res.stream().mapToInt(Integer::intValue).toArray();
+
+        for (int i = 1; i <= 9; i++) {
+            if (freq[i] == 0) continue;
+            freq[i]--;
+
+            for (int j = 0; j <= 9; j++) {
+                if (freq[j] == 0) continue;
+                freq[j]--;
+
+                for (int k = 0; k <= 8; k += 2) { // Only even digits
+                    if (freq[k] == 0) continue;
+                    freq[k]--;
+
+                    int num = i * 100 + j * 10 + k;
+                    result.add(num);
+
+                    freq[k]++;
+                }
+
+                freq[j]++;
+            }
+
+            freq[i]++;
+        }
+
+        Collections.sort(result);
+        int[] resArr = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resArr[i] = result.get(i);
+        }
+
+        return resArr;
     }
 }
