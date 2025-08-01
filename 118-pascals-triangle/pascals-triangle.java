9259b67491da530b7c170f3a1c69a3d7ff308d2e
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-08-01 11:23:15 +0530

    Time: 1 ms (82.66%) | Memory: 41.9 MB (91.00%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,23 +1,39 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle=new ArrayList<>();
-        List<Integer> first_Row=new ArrayList<>();
-         first_Row.add(1);
-         triangle.add(first_Row);
+        // List<List<Integer>> triangle=new ArrayList<>();
+        // List<Integer> first_Row=new ArrayList<>();
+        //  first_Row.add(1);
+        //  triangle.add(first_Row);
 
-         for(int i =1;i<numRows ;i++){
-            List<Integer> prev_Row=triangle.get(i-1);
-            List<Integer> curr_Row=new ArrayList<>();
+        //  for(int i =1;i<numRows ;i++){
+        //     List<Integer> prev_Row=triangle.get(i-1);
+        //     List<Integer> curr_Row=new ArrayList<>();
 
-            curr_Row.add(1);
-            for(int j =1;j<i;j++){
-                curr_Row.add(prev_Row.get(j-1)+prev_Row.get(j));
-            }
-            curr_Row.add(1);
-            triangle.add(curr_Row);
+        //     curr_Row.add(1);
+        //     for(int j =1;j<i;j++){
+        //         curr_Row.add(prev_Row.get(j-1)+prev_Row.get(j));
+        //     }
+        //     curr_Row.add(1);
+        //     triangle.add(curr_Row);
 
-         }
-         return triangle;
+        //  }
+        //  return triangle;
         
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i =0;i<numRows;i++){
+            List<Integer> row = new ArrayList<>(i+1);
+
+            for(int j =0;j<=i;j++){
+                if(j==0 || j==i)
+                row.add(1);
+                else{
+                    int num = result.get(i-1).get(j)+result.get(i-1).get(j-1);
+                    row.add(num);
+                }
+            }
+            result.add(row);
+        }
+        return result;
     }
 }
