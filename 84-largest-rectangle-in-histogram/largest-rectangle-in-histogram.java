d4fae60ac2b37a88e89e0845f14fa39c39a3eb8c
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-09-08 20:00:37 +0530

    Time: 110 ms (32.21%) | Memory: 62.1 MB (5.03%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,55 +1,43 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n =heights.length;
-        // we need two stack for nse and pse
-        Stack<Integer> st = new Stack<Integer>();
-        int[] nse = new int[n];
-         int[] pse = new int[n];
-         //calculating nse of all elements 
-         //will move from last - last ko stack mei push (index)
-         st.push(n-1);
-         nse[n-1]=n;
-         //loop from 2nd last element to 0 
-         for(int i =n-2;i>=0;i--){
-            //agar apne se bada element mil rha hai to pop
-            while(st.size()>0 && heights[st.peek()]>heights[i]){
-             st.pop();
+        int n = heights.length;
+        int[] right = new int[n];
+        int[] left = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        //next smallest right 
+         
+
+         for(int i =0;i<n;i++){
+            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
+                right[st.peek()]=i;
+                st.pop();
             }
-            //if stack is empty than write n 
-            if(st.size()==0) nse[i]=n;
-            else nse[i]=st.peek();
             st.push(i);
-        }
-          
-
-
-
-         //stack of empty karo 
-        while(st.size()>0) st.pop();
-            //calculate pse []- from start and first element will be 0
-
-       st.push(0);
-       pse[0]=-1;
-       for(int i =1;i<=n-1;i++){
-           while(st.size()>0 && heights[st.peek()]>=heights[i]){
-             st.pop();
+         }
+         while(!st.isEmpty()){
+            right[st.peek()]=n;
+            st.pop();
+         }
+        // right smallest right
+        
+         for(int i =n-1;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
+                left[st.peek()]=i;
+                st.pop();
             }
-             if(st.size()==0) pse[i]=-1;
-            else pse[i]=st.peek();
-              st.push(i);
-       }
+            st.push(i);
+         }
+        while(!st.isEmpty()){
+            left[st.peek()]=-1;
+            st.pop();
+         }
 
+         int ans =0;
+         for(int i =0;i<n;i++){
+           ans = Math.max(ans, heights[i] * (right[i] - left[i] - 1));  // ✅ Correct
 
-
-
-       //MAX AREA 
-       int max =-1;
-       for(int i =0;i<n;i++){
-int area =heights[i]*(nse[i]-pse[i]-1);
-max=Math.max(max,area);
-       }
-      
-return max;
-
+         }
+         return ans ;
     }
 }
