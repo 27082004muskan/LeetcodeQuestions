aba2f8fda90cff31451e4c76e7bb902ccce86ce8
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-09-11 20:52:41 +0530

    Time: 2 ms (54.89%) | Memory: 44.6 MB (95.42%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,18 +1,42 @@
+// class Solution {
+//     public int maxProduct(int[] nums) {
+      
+//         int n =nums.length;
+//         int maxProduct =Integer.MIN_VALUE;
+
+//         for(int i =0;i<n;i++){
+//             if(n==1) return nums[i];
+           
+//             int product =1;
+//         for(int j=i;j<n;j++){
+//              product *=nums[j];
+//              maxProduct = Math.max(product , maxProduct);
+//         }
+//         }
+//         return maxProduct;
+//     }
+// }
+
 class Solution {
     public int maxProduct(int[] nums) {
-      
-        int n =nums.length;
-        int maxProduct =Integer.MIN_VALUE;
-
-        for(int i =0;i<n;i++){
-            if(n==1) return nums[i];
-           
-            int product =1;
-        for(int j=i;j<n;j++){
-             product *=nums[j];
-             maxProduct = Math.max(product , maxProduct);
+        if (nums.length == 0) return 0;
+        
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            
+            // Store maxSoFar before updating
+            int tempMax = Math.max(curr, Math.max(maxSoFar * curr, minSoFar * curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar * curr, minSoFar * curr));
+            maxSoFar = tempMax;
+            
+            result = Math.max(result, maxSoFar);
         }
-        }
-        return maxProduct;
+        
+        return result;
     }
 }
+
